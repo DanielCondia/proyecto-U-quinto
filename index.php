6b0fa6f5238c6a06f7263d67192fff4c2211c411
Daniel Condía Figueredo <daniel.condia@gmail.com>
2024-09-02 20:36:36 -0500

    :sparkles: Se agrega primer ejemplo de index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<header>
+	<meta charset="UTF-8" />
+	<title>Lista de Tareas</title>
+</header>
+
+<body>
+	<h1>Lista de Tareas</h1>
+
+	<?php 
+	$numero_tareas = 10;
+	for ($i = 0; $i < $numero_tareas; $i++) {
+		echo("<p>Tarea número: $i</p>");
+	}
+	?>
+</body>
+
+
+</html>
