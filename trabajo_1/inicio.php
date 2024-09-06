8b2e18583a79909a44a5ecd078b2d5017449e36b
DanielCondia <daniel.condia@gmail.com>
2024-09-05 22:02:26 -0500

    :recycle: Correccion de redireccionamiento de pagina

--- trabajo_1/inicio.php
+++ trabajo_1/inicio.php
@@ -4,29 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container-fluid" id="principal">
-        <div class="row" style="background-image: url(https://e0.pxfuel.com/wallpapers/824/944/desktop-wallpaper-customer-centric-enterprise-work-project-management-solution-project-manager.jpg); background-repeat: no-repeat; background-position: center; background-size:cover; height: 100vh; width: 100vw;">
-            <div class="col-6"id="titulos">
-                <h1 style="color= #fffffff;">SGT</h1>
-                <h3>Gestiona tus tareas con facilidad</h3>
-            </div>
+<div class="container-fluid" id="principal">
+    <div class="row"
+         style="background-image: url(https://e0.pxfuel.com/wallpapers/824/944/desktop-wallpaper-customer-centric-enterprise-work-project-management-solution-project-manager.jpg); background-repeat: no-repeat; background-position: center; background-size:cover; height: 100vh; width: 100vw;">
+        <div class="col-6" id="titulos">
+            <h1 style="color= #fffffff;">SGT</h1>
+            <h3>Gestiona tus tareas con facilidad</h3>
         </div>
     </div>
-    <div class="container">
-        <div class="col">
-            <h2 style="text-align = center;">DE QUE TRATA?</h2>
-            <p>somos una pagina que te ayudara en la organizacion de tus tareas puedes agregar
-                cuantas quieras y darle un plazo de cumpliento con el titulo que quieras y con una nota en la que agregaras 
-                de que tratara la tarea que estas creando despues el sistema se encaragra de reportarte cunado 
-                fallaste con una tarea, tambien podras buscar alguna tarea si no la encuantras facil en la 
-                tabla donde veras las mas recinetes.
-            </p>
-            <button type="button" class="btn btn-outline-secondary" url="/trabajo_1/index.php">COMENZEMOS</button>
-        </div>
+</div>
+<div class="container">
+    <div class="col">
+        <h2 style="text-align = center;">DE QUE TRATA?</h2>
+        <p>somos una pagina que te ayudara en la organizacion de tus tareas puedes agregar
+            cuantas quieras y darle un plazo de cumpliento con el titulo que quieras y con una nota en la que agregaras
+            de que tratara la tarea que estas creando despues el sistema se encaragra de reportarte cunado
+            fallaste con una tarea, tambien podras buscar alguna tarea si no la encuantras facil en la
+            tabla donde veras las mas recinetes.
+        </p>
+        <button type="button" class="btn btn-outline-secondary" id="button-redireccionar">
+            COMENZEMOS
+        </button>
     </div>
+</div>
+
+<script>
+    const btn = document.getElementById("button-redireccionar");
+    btn.addEventListener('click', () => {
+        window.location.href = "index.php";
+    })
+</script>
+
 </body>
 </html>
